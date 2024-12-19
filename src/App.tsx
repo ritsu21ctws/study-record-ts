813f9b6392829f36f93be9bee8ea274de828ae55
ritsu <ritsu.21.ctws@gmail.com>
2024-12-19 18:55:12 +0900

    Chakra UIの導入

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,14 @@
-import './App.css';
+import { Button } from '@/components/ui/button';
+import { HStack } from '@chakra-ui/react';
 
 function App() {
   return (
     <>
       <div data-testid="title">タイトル</div>
+      <HStack>
+        <Button>Click me</Button>
+        <Button>Click me</Button>
+      </HStack>
     </>
   );
 }
