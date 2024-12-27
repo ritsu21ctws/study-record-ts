a0db14de293691742e82c26b8f84332bde36f4e5
ritsu <ritsu.21.ctws@gmail.com>
2024-12-27 18:43:05 +0900

    サンプルテストの修正

--- src/__tests__/App.spec.tsx
+++ src/__tests__/App.spec.tsx
@@ -1,9 +1,14 @@
 import App from '../App';
 import { render, screen } from '@testing-library/react';
+import { ChakraProvider, defaultSystem } from '@chakra-ui/react';
 
 describe('App', () => {
   test('タイトルがあること', async () => {
-    render(<App />);
+    render(
+      <ChakraProvider value={defaultSystem}>
+        <App />
+      </ChakraProvider>
+    );
     const title = screen.getByTestId('title');
     expect(title).toBeInTheDocument();
   });
